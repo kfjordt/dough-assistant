7b13111fe3f5eef41717ebbd7176733234b2f321
Kasper Jordt <kfjordt@gmail.com>
2023-11-24 19:17:32 +0100

    added easter egg

---
+++ frontend/src/components/other/EasterEgg.vue
@@ -0,0 +1,24 @@
+<template>
+    <div @click="handleClick()" class="easter-egg-container">
+        <img src="@/assets/brainstorming.png" alt="Easter egg" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleClick = () => {
+    router.push("login")
+}
+</script>
+
+<style scoped>
+.easter-egg-container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
