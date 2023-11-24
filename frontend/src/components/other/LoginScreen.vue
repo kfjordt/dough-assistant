ae1d33a6e7bf0277756a282bf953d358e8662719
Kasper Jordt <kfjordt@gmail.com>
2023-11-24 19:18:21 +0100

    implemented routing flow for components

---
+++ frontend/src/components/other/LoginScreen.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ApiService } from '../../api/ApiService';
+import { useUserStore } from '../../stores/user';
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const handleGoogleToken = (response: any) => {
+    const token = response.credential as string
+
+    ApiService.requestNewSession(token)
+        .then(userId => {
+            userStore.setLoggedInUserId(userId)
+            router.push("main")
+        })
+        .catch(error => console.error(error))
+}
+
+const handleSubheaderClick = () => {
+    router.push("brainstorming")
+}
+</script>
+
+<template>
+    <div class="login-container">
+        <div class="login">
+            <h1 class="auth-header">Dough Assistant</h1>
+            <h2 @click="handleSubheaderClick()" class="auth-subheader">Blazingly brisk budgeting</h2>
+            <GoogleLogin class="google-sign-in" :callback="handleGoogleToken" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.login-container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.login {
+    border-radius: 10px;
+    padding: 30px;
+    user-select: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: rgb(210, 210, 210);
+    background-color: rgb(50, 50, 50);
+}
+
+.auth-header,
+.auth-subheader {
+    margin: 0;
+    padding: 5px
+}
+
+.auth-subheader {
+    font-weight: normal;
+}
+
+.google-sign-in {
+    margin-top: 30px;
+}
+</style>
---
+++ frontend/src/components/other/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main-view">
+        <NavBar />
+        <Calendar />
+    </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from '../navBar/NavBar.vue';
+import Calendar from '../calendar/Calendar.vue';
+
+</script>
+
+<style scoped>
+.main-view {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+</style>
