e1f0a9fb95a83c77cd5b068ec491b83099cb58ad
Kasper Jordt <kfjordt@gmail.com>
2023-11-24 19:17:51 +0100

    added routing

--- frontend/src/components/DoughAssistantApp.vue
+++ frontend/src/components/DoughAssistantApp.vue
@@ -1,60 +1,31 @@
 <script setup lang="ts">
+import {  onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { useUserStore } from '../stores/user';
-import { ref, onMounted } from 'vue';
-import Calendar from './calendar/Calendar.vue';
-import Authentication from './other/Authentication.vue';
-import Tooltip from './other/Tooltip.vue';
-import Loading from './other/Loading.vue';
-import NavBar from './navBar/NavBar.vue';
 
-const userStore = useUserStore()
-const isLoading = ref(true);
+const router = useRouter()
+const loggedInUserId = useUserStore().loggedInUserId
 
 onMounted(() => {
-    // Here, implement the method described in
-    // https://stackoverflow.com/questions/244882/what-is-the-best-way-to-implement-remember-me-for-a-website
-    isLoading.value = false
+    const isUserLoggedIn = loggedInUserId != null
+    router.push(
+        isUserLoggedIn
+            ? "main"
+            : "login"
+    )
 })
+
 </script>
 
 <template>
     <div class="app-container">
-        <Loading v-if="isLoading" class="app-loading-screen" />
-        <div v-else class="app">
-            <div class="main-view" v-if="userStore.loggedInUserId !== null">
-                <NavBar />
-                <Calendar />
-            </div>
-            <Authentication v-else />
-        </div>
-        <Tooltip />
+        <router-view></router-view>
     </div>
 </template>
 
 <style scoped>
 .app-container {
     font-family: 'Segoe UI', 'Arial Narrow', Arial, sans-serif;
-}
-
-.app {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     background-color: rgb(30, 30, 30);
-}
-
-.app-loading-screen {
-    width: 20%;
-    height: 20%;
-}
-
-.main-view {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    user-select: none;
 }
 </style>
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { OhVueIcon, addIcons } from "oh-vue-icons"
 import { allIcons } from './models/icons/Icons';
-import DoughAssistantApp from './components/DoughAssistantApp.vue';
 import vue3GoogleLogin from 'vue3-google-login'
 import { clientSecrets } from './ClientSecrets';
+import DoughAssistantApp from './components/DoughAssistantApp.vue';
+import MainView from './components/other/MainView.vue';
+import LoginScreen from './components/other/LoginScreen.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import EasterEgg from "./components/other/EasterEgg.vue"
 
 allIcons.forEach(icon => {
     addIcons(icon);
@@ -14,10 +18,18 @@ const app = createApp(DoughAssistantApp)
 
 app.component("v-icon", OhVueIcon);
 
-app.use(vue3GoogleLogin, {
-    clientId: clientSecrets.googleClientId
-  })
-  
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/main', component: MainView, name: "main" },
+        { path: '/login', component: LoginScreen, name: "login" },
+        { path: '/brainstorming', component: EasterEgg, name: "brainstorming" }
+    ]
+})
+
+app.use(router)
+app.use(vue3GoogleLogin, { clientId: clientSecrets.googleClientId })
 app.use(createPinia())
+
 app.mount('#app')
 
