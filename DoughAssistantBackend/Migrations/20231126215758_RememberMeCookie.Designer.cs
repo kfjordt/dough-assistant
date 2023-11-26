50030febdec342d10e5bf92ee7af4456687942f2
Kasper Jordt <kfjordt@gmail.com>
2023-11-26 22:59:51 +0100

    db migrations for remember me

--- DoughAssistantBackend/Migrations/20231126215758_RememberMeCookie.Designer.cs
+++ DoughAssistantBackend/Migrations/20231126215758_RememberMeCookie.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DoughAssistantBackend.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DoughAssistantBackend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231126215758_RememberMeCookie")]
+    partial class RememberMeCookie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
